1edec88ed6eed0edbb029e69e0cc90ae602ba219
Livia <livia.namba@gmail.com>
2023-05-18 22:01:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//alerta para o usuário abrir o app e ver o novo caminho gerado após algum perigo ser detectado
+
+const notificationButton = document.getElementById('notificationButton');
+
+notificationButton.addEventListener('click', function() {
+  alert('PERIGO PRÓXIMO!!! ENTRE NO APP PARA VERIFICAR O NOVO TRAJETO!!!');
+  // Coloque sua lógica adicional aqui
+});
